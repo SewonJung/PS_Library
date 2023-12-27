3d83a94341b7b87ab20619d13ad8ab20285824cb
SewonJung <94056835+SewonJung@users.noreply.github.com>
2023-12-27 09:38:40 +0900

    Create default.cpp

---
+++ 0. Basic /default.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}
